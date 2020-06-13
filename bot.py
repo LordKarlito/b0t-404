55b9eff06b7b20b342d524f0800c35845d8f2be2
Karlo Barcelona <krlbrcln12@gmail.com>
2020-06-13 22:20:29 +0800

    updated comments

--- bot.py
+++ bot.py
@@ -5,18 +5,29 @@ import os
 
 # Function to create a certificate with the user's name
 def cert_gen(name):
+    # Create an image object
     img = Image.open("images/WC_Cert.png")
 
+    # Creating the "canvas" where the text will be entered
     text_container = Image.new('RGBA', (448, 90), (253,253,253))
-    font = ImageFont.truetype("assets/BebasKai.ttf", 40)
-    w,h=font.getsize(str(name))
 
+    font = ImageFont.truetype("assets/BebasKai.ttf", 40)
+
+    # Get font size and make some computations to make the text centered horizontally and vertically in the specified canvass size
+    w,h=font.getsize(str(name))
     pointw = int((448 - w)/2)
     pointh = int((95 - h)/2)
+
+    # Start "drawing" on the canvas
     text_draw = ImageDraw.Draw(text_container)
+
+    # The thing to draw, in this case, the text
     text_draw.text((pointw, pointh), name, font = font, fill="black")
+
+    # Paste the canvas on top of our original image
     img.paste(text_container, (225,190))
 
+    # Create the output file
     img.save("images/output.png", "PNG")
     output_file = "images/output.png"
 
@@ -33,8 +44,5 @@ async def on_message(message):
         # Send the result of the funtion as a file
         await message.channel.send(file=discord.File(cert_gen(message.author.name)))
 
-@commands.is_owner()
-async def shutdown(ctx):
-    await ctx.bot.logout()
-
-client.run("NzIxMDI4OTAzODA3ODc3MTgw.XuOkiQ.KioOHvNi1iARfykSb0qRD7WUgXg")
+# Your key here!
+client.run("")
